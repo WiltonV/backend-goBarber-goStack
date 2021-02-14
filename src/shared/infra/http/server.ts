83ee7bddaaf304baa0dd686a83c412dba2574d06
Wilton Vieira <wlv.wilton@gmail.com>
2021-02-14 12:30:03 -0300

    Fixing request types

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -35,4 +35,4 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     .json({ status: 'error', message: 'Internal server error' });
 });
 
-app.listen(3333, () => console.log('Server started on port 3333!'));
+app.listen(3333, () => console.log('🚀  Server started on port 3333!'));
